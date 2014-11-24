6cc175fd666b23cdda3b457305878395188236e0
Bernhard Sieglin <Bernhard.Sieglin@ipp.mpg.de>
2014-11-24 14:41:32 +0100

    Added further documentation.

--- include/fieldline/core/magneticField.hpp
+++ include/fieldline/core/magneticField.hpp
@@ -5,15 +5,30 @@
 #include <iostream>
 #include <math.h>
 
+/*! Class containing one magnetic field vector
+    This class contains the magnetic field vector.
+*/ 
+
 namespace Fieldline {
     namespace core {
         class magneticField {
             public:
+                /*! Default constructor
+                    The default constructor initialized the instance without magnetic field. All components are zero.
+                */
                 magneticField() : BR(0.0), Bz(0.0), Btor(0.0) {}
+                /*! Initializing constructor
+                    This constructor takes the magnetic field components in R,z and toroidal direction.
+                */
                 magneticField(const double br, const double bz, const double btor) : BR(br), Bz(bz), Btor(btor) {}
+                /*! Copy constructor */
                 magneticField(const magneticField & rhs) : BR(rhs.BR), Bz(rhs.Bz), Btor(rhs.Btor) {}
+                /*! Destructor */
                 virtual ~magneticField() {}
 
+                /*! Assignment operator
+                    This operator copies the magnetic field vector from the given instance.
+                */
                 magneticField & operator= (const magneticField & rhs) {
                     if(this != &rhs) {
                         BR = rhs.BR;
@@ -22,14 +37,23 @@ namespace Fieldline {
                     }
                     return *this;
                 }
+                /*! Get total magnetic field
+                    This function returns the total magnetic field \f$B_{tot} = \sqrt{B_R^2 + B_z^2 + B_{tor}^2}\f$
+                */
                 double Btot() const {
                     return sqrt(BR*BR + Bz*Bz + Btor*Btor);
                 }
+                /*! Get poloidal magnetic field
+                    This function returns the poloidal magnetic field \f$B_{pol} = \sqrt{B_R^2 + B_z^2} \f$ 
+                */
                 double Bpol() const {
                     return sqrt(BR*BR + Bz*Bz);
                 }
+                /*! Magnetic field component in R direction. */
                 double BR;
+                /*! Magnetic field component in z direction. */
                 double Bz;
+                /*! Magnetic field component in toroidal direction. */
                 double Btor;
 
         };
--- include/fieldline/core/stopCriterion.hpp
+++ include/fieldline/core/stopCriterion.hpp
@@ -3,10 +3,23 @@
 
 #include <fieldline/core/fieldline.hpp>
 
+/*! Stop Criterion
+    Abstract class for the definition of stop criteria for the field line trace.
+    This class should only be used as a parent for the designated stop criteria.
+    Never use an instance of this class, only use its children.
+
+*/
+
 namespace Fieldline {
     namespace core {
         class stopCriterion {
             public:
+                /*! Stop criterion reached.
+                    This function is defined as an abstract function.
+                    In the derived classes this function has to be overridden with the stop criteria.
+                    If the stop criteria is reached the function has to return true.
+                    If the criteria is not met the field line trace has to continue and the function has to return false.
+                */
                 virtual bool reached(fieldline * rhs) = 0;
         };
     }
