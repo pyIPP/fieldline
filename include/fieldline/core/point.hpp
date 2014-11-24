a7dc80e1867f2ae951f449b405f26fef5edced69
Bernhard Sieglin <Bernhard.Sieglin@ipp.mpg.de>
2014-11-24 11:12:04 +0100

    Started to add documentation.
    Documentation for
    
    Fieldline::core::point
    Fieldline::core::line
    
    added.

--- include/fieldline/core/point.hpp
+++ include/fieldline/core/point.hpp
@@ -4,14 +4,30 @@
 #include <iostream>
 #include <math.h>
 
+//! point class
+/*!
+    This class contains the information about a point in the poloidal plain.
+*/
+
 namespace Fieldline {
     namespace core {
         class point {
             public:
+                //! Default constructor
                 point() : R(0.0), z(0.0), hit(false) {}
+                //! A constructor
+                /*!
+                    Initialized the point with the given R,z coordinate and the information if it got hit.
+                */
                 point(const double RIn, const double zIn, const bool hitIn) : R(RIn), z(zIn), hit(hitIn) {}
+                //! Copy constructor
                 point(const point & rhs) : R(rhs.R), z(rhs.z), hit(rhs.hit) {}
+                //! Destructor
                 virtual ~point() {}
+                //! Copy operator
+                /*!
+                    This operator copies the information of the point into the given instance.
+                */
                 point & operator= (const point & rhs) {
                     if(this != &rhs) {
                         R = rhs.R;
@@ -20,11 +36,17 @@ namespace Fieldline {
                     }
                     return *this;
                 }
-
-
+                //! R value of the point.
                 double R;
+                //! z value of the point.
                 double z;
+                //! Information if point was hit.
                 bool hit;
+                //! Distance between two points.
+                /*!
+                    This function calculates the distance in the poloidal plain between two points.
+                    \f$ d = \sqrt{(R_1 - R_0)^2 + (z_1 - z_0)^2} \f$
+                */
                 double get_distance(const point & rhs) const {
                     return sqrt(pow(R-rhs.R,2) + pow(z-rhs.z,2));
                 }
